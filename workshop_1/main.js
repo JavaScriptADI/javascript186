51caffcd2a62edd2c5436603c41248e402612421
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2025-01-22 21:19:38 +0400

    Add workshop 1 code

---
+++ workshop_1/main.js
@@ -0,0 +1,11 @@
+let money_in_wallet = 100;
+let sandwich_price = 10;
+
+console.log("Money in wallet: $" + money_in_wallet);
+
+console.log("Sandwich price: $" + sandwich_price);
+
+// money_in_wallet = money_in_wallet - sandwich_price; // 90
+money_in_wallet -= sandwich_price;
+
+console.log("Money left in wallet: $" + money_in_wallet);
