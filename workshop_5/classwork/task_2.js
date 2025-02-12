1e815aad9078c20a267c062ea15352763a344fe0
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2025-02-12 19:58:13 +0400

    Add classwork for workshop 5

--- workshop_5/classwork/task_2.js
+++ workshop_5/classwork/task_2.js
@@ -7,3 +7,29 @@
  * 
  * also show user which day is he wiritng for
  */
+
+const temperatures = [];
+
+
+while (true) {
+    const userInput = prompt(`Enter temperature for day ${temperatures.length + 1} [-100, 100] (enter \"q\" to stop recording)`)
+
+    if (userInput === "q") {
+        break;
+    }
+
+    const temperature = Number(userInput);
+    if (-100 <= temperature && temperature <= 100) {
+        temperatures.push(temperature);
+        continue
+    }
+    console.log("Please ensure temperature is in correct range [-100, 100]");
+}
+
+console.log(temperatures);
+let totalTemp = 0;
+for (let i = 0; i < temperatures.length; i++) {
+    totalTemp += temperatures[i];
+}
+
+console.log(`Average temperature is ${totalTemp / temperatures.length}`);
--- workshop_5/classwork/task_3.js
+++ workshop_5/classwork/task_3.js
@@ -4,3 +4,19 @@
  * change every occurence of n with random number
  * if changed number is divisible by n remove it from array
  */
+const n = 27;
+const nums = [];
+let i = 0;
+
+while (nums.length < 1000) {
+    i++;
+    const randomNumber = parseInt(Math.random() * 101);
+
+    if (randomNumber % n !== 0) {
+        nums.push(randomNumber);
+    }
+}
+
+console.log(nums.length);
+console.log(`Generating this list took ${i} iterations`);
+console.log(`${n} is in nums ${nums.includes(n)}`);
